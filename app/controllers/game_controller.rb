0076a6b637ee58b73b557b9496e587c03b0a7a44
felix24mihai <mihai_prava@yahoo.com>
2022-11-22 04:01:03 +0200

    Update text & images

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -13,13 +13,13 @@ class GameController < ApplicationController
             curb_choice = JSON.parse(res.body)["body"]
         end
         result = game_winner(params['user_choice'], curb_choice)
-        render json: {choice: curb_choice, result: result}
+        render json: {choice: curb_choice, result: result[0], opponent_result: result[1]}
     end
 
     private
 
     def game_winner(user, curb)
-        return 'draw' if user == curb
+        return ["IT'S A DRAW!", "Curb with #{curb} drew"] if user == curb
         resource_value = {
             'rock': 1,
             'paper': 2,
@@ -27,7 +27,7 @@ class GameController < ApplicationController
         }
         user_val = resource_value[user.to_sym]
         curb_val = resource_value[curb.to_sym]
-        return 'lose' if user_val == 3 && curb_val == 1 || user_val + 1 == curb_val
-        return 'won'
+        return ['YOU LOST!', "Curb with #{curb} won"] if user_val == 3 && curb_val == 1 || user_val + 1 == curb_val
+        return ['YOU WON!', "Curb with #{curb} lost"]
     end
 end
