783f3f04a74063925ab3b12ca27a4159ed64f323
felix24mihai <mihai_prava@yahoo.com>
2022-11-22 03:05:48 +0200

    Add game logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,33 @@
 class GameController < ApplicationController
+    require 'uri'
+    require 'net/http'
+
     def play_game
+    end
+
+    def curb_response
+        uri = URI('https://5eddt4q9dk.execute-api.us-east-1.amazonaws.com/rps-stage/throw')
+        res = Net::HTTP.get_response(uri)
+        curb_choice = ['rock', 'paper', 'scissors'].sample
+        if res.is_a?(Net::HTTPSuccess)
+            curb_choice = JSON.parse(res.body)["body"]
+        end
+        result = game_winner(params['user_choice'], curb_choice)
+        render json: {choice: curb_choice, result: result}
+    end
+
+    private
+
+    def game_winner(user, curb)
+        return 'draw' if user == curb
+        resource_value = {
+            'rock': 1,
+            'paper': 2,
+            'scissors': 3
+        }
+        user_val = resource_value[user.to_sym]
+        curb_val = resource_value[curb.to_sym]
+        return 'lose' if user_val == 3 && curb_val == 1 || user_val + 1 == curb_val
+        return 'won'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "game#play_game"
+  root 'game#play_game'
+  get '/game/result', to: "game#curb_response"
 end
